7a2138587b29dd5c24e0c5bd54b681a5de6838c8
Fırat Soysal <firat.soysal@lifemote.com>
2023-09-19 19:39:39 +0300

    Let's start!

---
+++ sonic.py
@@ -0,0 +1,42 @@
+import speedtest
+from datetime import datetime
+import time as t
+import matplotlib.pyplot as plt
+
+time_list = []
+download_list = []
+
+
+def plot_graph(x_axis, y_axis, date):
+    plt.plot(x_axis, y_axis, "--bo")
+    plt.title("Speedtest")
+    plt.xlabel("Date")
+    plt.ylabel("Download(Mbit)")
+    plt.savefig("speedtest_" + str(date) + ".png")
+    plt.show()
+
+
+def write_data(arr, file_name):
+    with open(file_name + ".txt", "w") as file:
+        for value in arr:
+            file.write(str(value) + "\n")
+
+
+try:
+    while True:
+        print("Started to test. Please do not interrupt!")
+        time = datetime.now()
+        print(time)
+
+        download_list.append(speedtest.Speedtest().download()/1000000)
+        time_list.append(time)
+        print("Test ended successfully")
+
+        t.sleep(900)
+except KeyboardInterrupt:
+    write_data(download_list, "download_values")
+    write_data(time_list, "times")
+    plot_graph(time_list, download_list, time)
+
+
+
