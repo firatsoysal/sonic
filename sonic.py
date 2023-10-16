5c9569e724b64f05921807e9caa7f738f65002b0
Fırat Soysal <firat.soysal@lifemote.com>
2023-10-16 15:33:04 +0300

    Added exception

--- sonic.py
+++ sonic.py
@@ -10,15 +10,20 @@ def write_to_csv(data):
 
 
 while True:
-    print("Started to test. Please do not interrupt!")
+    try:
+        print("Started to test. Please do not interrupt!")
 
-    dl_speed = speedtest.Speedtest(secure=True).download()/1000000
-    test_time = datetime.now()
-    data_to_write = [test_time, dl_speed]
-    write_to_csv(data_to_write)
-    print(dl_speed)
-    print("Test ended successfully")
+        dl_speed = speedtest.Speedtest(secure=True).download()/1000000
+        test_time = datetime.now()
+        data_to_write = [test_time, dl_speed]
+        write_to_csv(data_to_write)
+        print(dl_speed)
+        print("Test ended successfully")
 
-    t.sleep(30)
+        t.sleep(30)
+    except Exception as e:
+        with open('errors.txt', 'a') as error_file:
+            error_file.write(f"An error occurred: {str(e)}\n")
+        continue
 
 
